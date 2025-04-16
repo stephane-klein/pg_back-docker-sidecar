7010ac5a409d4e0600322f13fd43b8ac95ec921f
stephane-klein <contact@stephane-klein.info>
2025-04-16 11:06:22 +0200

    Fix bug, add --if-exists missing parameter in pg_restore

--- scripts/pg_back2-import-dump.sh
+++ scripts/pg_back2-import-dump.sh
@@ -6,6 +6,6 @@ cd "$(dirname "$0")/../"
 docker compose exec -T pg_back2 sh -c "cat << 'EOF' | sh
 pg_back --download s3 foobar/*${@}* --decrypt \"*\"
 psql -U \$POSTGRES_USER -p \$POSTGRES_PORT -h \$POSTGRES_HOST \$POSTGRES_DB -f /var/backups/postgresql/foobar/pg_globals_${@}Z.sql > /dev/null 2>&1
-pg_restore -c -U \$POSTGRES_USER -p \$POSTGRES_PORT -h \$POSTGRES_HOST -d \$POSTGRES_DBNAME /var/backups/postgresql/foobar/postgres_${@}Z.dump > /dev/null 2>&1
+pg_restore -c --if-exists -U \$POSTGRES_USER -p \$POSTGRES_PORT -h \$POSTGRES_HOST -d \$POSTGRES_DBNAME /var/backups/postgresql/foobar/postgres_${@}Z.dump > /dev/null 2>&1
 rm -rf /var/backups/postgresql/
 EOF"

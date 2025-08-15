e6c63d4f02719c75b18d8eb7a01e813d10875bff
ItsAlex <me@itsalex.fr>
2025-08-15 21:01:44 +0200

    fix: allow restoration of database if not exist when executing pg_restore

--- scripts/postgres2-import-local-dump.sh
+++ scripts/postgres2-import-local-dump.sh
@@ -8,6 +8,6 @@ docker compose cp ./tmp-downloads-dump/postgres_${@}Z.dump postgres2:/postgres.d
 
 docker compose exec -T postgres2 sh -c "cat << 'EOF' | sh
 psql -U \$POSTGRES_USER \$POSTGRES_DB -f /pg_globals.sql > /dev/null 2>&1
-pg_restore -c --if-exists -U \$POSTGRES_USER -d \$POSTGRES_DB /postgres.dump > /dev/null 2>&1
+pg_restore -c --if-exists -C -U \$POSTGRES_USER -d \$POSTGRES_DB /postgres.dump > /dev/null 2>&1
 rm /pg_globals.sql /postgres.dump
 EOF"

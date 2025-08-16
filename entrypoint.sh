629829ed639d8e8e06d1b381cbd178752e0a5286
stephane-klein <contact@stephane-klein.info>
2025-08-16 16:39:51 +0200

    Set up environment variables to let aws cli access to the object storage environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,13 @@ gomplate --file /pg_back.conf.tmpl --out /etc/pg_back/pg_back.conf
 echo "${POSTGRES_HOST}:${POSTGRES_PORT}:*:${POSTGRES_USER}:${POSTGRES_PASSWORD}" > ~/.pgpass
 chmod 0600 ~/.pgpass
 
+# Set up environment variables to let aws cli access to the object
+# storage environment that has the backups made by pg_back
+export AWS_ACCESS_KEY_ID="${S3_KEY_ID}"
+export AWS_SECRET_ACCESS_KEY="${S3_SECRET}"
+export AWS_ENDPOINT_URL="${S3_ENDPOINT}"
+export AWS_DEFAULT_REGION="${S3_REGION}"
+
 if [[ $# -ne 0 ]]; then
     exec "$@"
     exit 0
